3a3309feb8d8b47b9f2e439e25d5496e8e6c9b78
Rėdas Peškaitis <redas.peskaitis@gmail.com>
2021-04-08 10:26:17 +0300

    Rename main .js file

--- webpack.mix.js
+++ webpack.mix.js
@@ -17,7 +17,7 @@ mix
     .webpackConfig({ plugins: [new ESLintPlugin()] })
     .sourceMaps(false, 'source-map')
     .copyDirectory('resources/_public', 'public')
-    .js('resources/js/app.js', 'public/js')
+    .js('resources/js/main.js', 'public/js')
     .postCss('resources/css/app.css', 'public/css', [])
     .react()
     .browserSync({ proxy: '127.0.0.1:8000', ui: false });

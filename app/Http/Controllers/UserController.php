b9623663ab74b8e79aba390839fb599a9ae888de
Karolis-Kvatavicius <karolis.kvatavicius@gmail.com>
2021-04-08 19:31:41 +0300

    user dashboard v0.1
    user dashboard

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -2,88 +2,33 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
 
 class UserController extends Controller
 {
     public function __construct()
     {
-        $this->middleware(['auth:sanctum'])->only(['logout']);
+        // $this->middleware(['auth:sanctum']);
     }
-
     /**
-     * Display a listing of the resource.
+     * Get info of logged in user.
      *
      * @return \Illuminate\Http\Response
      */
-    public function index()
+    public function getUserInfo(User $user)
     {
-        //
+        return $user;
     }
 
     /**
-     * Show the form for creating a new resource.
+     * Update user info.
      *
      * @return \Illuminate\Http\Response
      */
-    public function create()
+    public function updateUserInfo(Request $request, User $user)
     {
-        //
-    }
-
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
-    public function store(Request $request)
-    {
-        //
-    }
-
-    /**
-     * Display the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function show($id)
-    {
-        //
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function edit($id)
-    {
-        //
-    }
-
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function update(Request $request, $id)
-    {
-        //
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function destroy($id)
-    {
-        //
+        $user->update($request->all());
+        return "Updated";
     }
 }
